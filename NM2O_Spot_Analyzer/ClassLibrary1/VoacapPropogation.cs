3db52a8786f27f7c86185f40c8db2e2463b2bf1e
Siphonophora <michael.j.conrad+github@gmail.com>
2018-11-22 15:54:30 -0500

    Add error message.

--- NM2O_Spot_Analyzer/ClassLibrary1/VoacapPropogation.cs
+++ NM2O_Spot_Analyzer/ClassLibrary1/VoacapPropogation.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Utility;
 
 namespace CallParser
@@ -33,43 +34,51 @@ namespace CallParser
         /// <returns></returns>
         private List<Propogation> ParsePropogations()
         {
-            List<Propogation> newProps = new List<Propogation>();
-
-            //TODO the program will fail if the file doesn't exist
-            string file = @"C:\itshfbc\run\nm2o_voacap.out";
-            var fileInfo = new FileInfo(file);
-            PropDate = fileInfo.LastWriteTime;
-            string[] rows = File.ReadAllLines(file);
-
-            string country = "";
-            string path = "";
-            int hour = 0;
-
-
-            for (int i = 0; i < rows.LongLength; i++)
+            try
             {
-                string row = rows[i].PadRight(80); //Pad this so we don't get errors on the substring being out of range. 
-                if (row.Substring(62, 6).Equals("N. MI."))
-                {
-                    path = row.Substring(2, 1);
-                    country = row.Substring(22, 20).TrimEnd();
-                }
-                else if (row.Substring(67, 4).Equals("FREQ"))
-                {
-                    hour = int.Parse(row.Substring(2, 2));
-                }
-                else if (row.Substring(67, 3).Equals("REL"))
-                {
-                    newProps.Add(new Propogation(country, hour, path, float.Parse(row.Substring(12, 4)), RadioInfo.BandName.OneSixtyMeters));
-                    newProps.Add(new Propogation(country, hour, path, float.Parse(row.Substring(17, 4)), RadioInfo.BandName.EightyMeters));
-                    newProps.Add(new Propogation(country, hour, path, float.Parse(row.Substring(22, 4)), RadioInfo.BandName.FourtyMeters));
-                    newProps.Add(new Propogation(country, hour, path, float.Parse(row.Substring(27, 4)), RadioInfo.BandName.TwentyMeters));
-                    newProps.Add(new Propogation(country, hour, path, float.Parse(row.Substring(32, 4)), RadioInfo.BandName.FifteenMeters));
-                    newProps.Add(new Propogation(country, hour, path, float.Parse(row.Substring(37, 4)), RadioInfo.BandName.TenMeters));
-                }
-            }
+                List<Propogation> newProps = new List<Propogation>();
 
-            return newProps;
+                //TODO the program will fail if the file doesn't exist
+                string file = @"C:\itshfbc\run\nm2o_voacap.out";
+                var fileInfo = new FileInfo(file);
+                PropDate = fileInfo.LastWriteTime;
+                string[] rows = File.ReadAllLines(file);
+
+                string country = "";
+                string path = "";
+                int hour = 0;
+
+
+                for (int i = 0; i < rows.LongLength; i++)
+                {
+                    string row = rows[i].PadRight(80); //Pad this so we don't get errors on the substring being out of range. 
+                    if (row.Substring(62, 6).Equals("N. MI."))
+                    {
+                        path = row.Substring(2, 1);
+                        country = row.Substring(22, 20).TrimEnd();
+                    }
+                    else if (row.Substring(67, 4).Equals("FREQ"))
+                    {
+                        hour = int.Parse(row.Substring(2, 2));
+                    }
+                    else if (row.Substring(67, 3).Equals("REL"))
+                    {
+                        newProps.Add(new Propogation(country, hour, path, float.Parse(row.Substring(12, 4)), RadioInfo.BandName.OneSixtyMeters));
+                        newProps.Add(new Propogation(country, hour, path, float.Parse(row.Substring(17, 4)), RadioInfo.BandName.EightyMeters));
+                        newProps.Add(new Propogation(country, hour, path, float.Parse(row.Substring(22, 4)), RadioInfo.BandName.FourtyMeters));
+                        newProps.Add(new Propogation(country, hour, path, float.Parse(row.Substring(27, 4)), RadioInfo.BandName.TwentyMeters));
+                        newProps.Add(new Propogation(country, hour, path, float.Parse(row.Substring(32, 4)), RadioInfo.BandName.FifteenMeters));
+                        newProps.Add(new Propogation(country, hour, path, float.Parse(row.Substring(37, 4)), RadioInfo.BandName.TenMeters));
+                    }
+                }
+
+                return newProps;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Failed to load propogations. Try running them again");
+                return null;
+            }
         }
 
         public Propogation CurrentPropogation(string countryFixedName, RadioInfo.BandName band)
